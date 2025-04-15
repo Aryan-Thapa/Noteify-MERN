bfb3d1c62ad85ff511b8f40d94bc1dcbb64e99f4
Aryan-Thapa <aryanthapa219@gmail.com>
2025-04-16 00:44:04 +0530

    Protected Route added

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -4,7 +4,7 @@ import Login from "./pages/login";
 import Signup from "./pages/signup";
 import Dashboard from "./pages/Dashboard";
 import Settings from "./pages/Settings";
-import DashboardLayout from "./layouts/DashboardLayout";
+import PrivateRoute from "./components/PrivateRoute";
 import Landing from "./pages/Landing";
 function App() {
   return (
@@ -14,7 +14,7 @@ function App() {
         <Route path="/login" element={<Login />} />
         <Route path="/signup" element={<Signup />} />
 
-        <Route element={<DashboardLayout />}>
+        <Route element={<PrivateRoute />}>
           <Route path="/dashboard" element={<Dashboard />} />
           <Route path="/settings" element={<Settings />} />
         </Route>
