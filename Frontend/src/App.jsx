37598c5ab4425ac716a42c64628689ec2931c0c6
Aryan-Thapa <aryanthapa219@gmail.com>
2025-05-04 22:47:44 +0530

    Typo FIX

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,7 +1,7 @@
 import "./App.css";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Login from "./pages/login";
-import Signup from "./pages/signup";
+import Login from "./pages/Login";
+import Signup from "./pages/Signup";
 import Dashboard from "./pages/Dashboard";
 import Settings from "./pages/Settings";
 import PrivateRoute from "./components/PrivateRoute";
